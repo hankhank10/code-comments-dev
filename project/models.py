aa519637145f035ed9c860f12053bb232bb03f53
hankhank10 <m.a.hankinson@gmail.com>
2021-06-29 13:12:05 +0100

    Update database model

--- project/models.py
+++ project/models.py
@@ -4,7 +4,7 @@ from flask_login import UserMixin
 
 
 class User(UserMixin, db.Model):
-    id = db.Column(db.Integer, primary_key=True)  # primary keys are required by SQLAlchemy
+    id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(100))
     username = db.Column(db.String(100))
@@ -12,6 +12,10 @@ class User(UserMixin, db.Model):
     # setup strings
     verified = db.Column(db.Boolean)
     unique_setup_key = db.Column(db.String(30))
+
+    snapshots = db.relationship('Snapshot', backref='owner', lazy=True)
+    comments = db.relationship('Comment', backref='author', lazy=True)
+
 
     def __repr__(self):
         return self.id
@@ -22,10 +26,13 @@ class Snapshot(db.Model):
     repo_author = db.Column(db.String[50])
     repo_name = db.Column(db.String[100])
     unique_suffix = db.Column(db.String[50])
-
     url = db.Column(db.String[300])
 
+    owner_id = db.Column(db.ForeignKey('user.id'), nullable = False)
+
     date_snapped = db.Column(db.DateTime)
+
+    gists = db.relationship('Gist', backref='snapshot', lazy=True)
 
     def unique_reference(self):
         return self.repo_author + "*" + self.repo_name + "*" + self.unique_suffix
@@ -33,16 +40,17 @@ class Snapshot(db.Model):
     def __repr__(self):
         return self.unique_reference()
 
-    owner = db.Column(db.ForeignKey('user.id'), nullable = False)
 
 
 class Gist(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    snapshot = db.Column(db.ForeignKey('snapshot.id'), nullable = False)
+    snapshot_id = db.Column(db.ForeignKey('snapshot.id'), nullable = False)
     filename = db.Column(db.String[50])
     url = db.Column(db.String[300])
     downloaded = db.Column(db.Boolean)
     content = db.Column(db.String[20000])
+
+    lines = db.relationship('Line', backref='gist', lazy=True)
 
     def __repr__(self):
         return self.id
@@ -50,9 +58,11 @@ class Gist(db.Model):
 
 class Line(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    gist = db.Column(db.ForeignKey('gist.id'), nullable = False)
+    gist_id = db.Column(db.ForeignKey('gist.id'), nullable = False)
     content = db.Column(db.String[1000])
     line_number = db.Column(db.Integer)
+
+    comments = db.Relationship('Comment', backref='line', lazy=True)
 
     def __repr__(self):
         return self.id
@@ -60,6 +70,6 @@ class Line(db.Model):
 
 class Comment(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    line = db.Column(db.ForeignKey('line.id'), nullable = False)
-    owner = db.Column(db.ForeignKey('user.id'), nullable = False)
+    line_id = db.Column(db.ForeignKey('line.id'), nullable = False)
+    owner_id = db.Column(db.ForeignKey('user.id'), nullable = False)
     content = db.Column(db.String[1000])
